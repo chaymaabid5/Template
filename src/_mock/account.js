5361ea4f8c2316214ade3cc9d75a474ef84c4d5b
chayma.abid <chaymaabid446@gmail.com>
2024-04-05 15:35:38 +0200

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Chayma Abid',
+  email: 'chaymaabid446@gmail.com',
+  photoURL: '/assets/illustrations/girl.jpg',
+};
